7889e03637eb4cd48e62aaa272e9fbc924db33fd
prlnd <palffy.rolland123@gmail.com>
2021-01-08 04:51:04 +0200

    Call and Page button fixed.

--- app/src/main/java/com/example/wheretoeat/ui/fragments/overview/OverviewFragment.kt
+++ app/src/main/java/com/example/wheretoeat/ui/fragments/overview/OverviewFragment.kt
@@ -1,6 +1,8 @@
 package com.example.wheretoeat.ui.fragments.overview
 
 import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -9,6 +11,7 @@ import android.view.ViewGroup
 import com.bumptech.glide.Glide
 import com.example.wheretoeat.R
 import com.example.wheretoeat.model.Restaurant
+import kotlinx.android.synthetic.main.fragment_overview.*
 import kotlinx.android.synthetic.main.fragment_overview.view.*
 
 class OverviewFragment : Fragment() {
@@ -33,6 +36,20 @@ class OverviewFragment : Fragment() {
         view.addressDetail_textView.text =
             "${myBundle?.address},\n${myBundle?.city}, ${myBundle?.country}"
         view.phone_textView.text = myBundle?.phone
+
+        view.call_button.setOnClickListener {
+            val intent = Intent()
+            intent.action = Intent.ACTION_DIAL
+            intent.data = Uri.parse("tel:" + myBundle?.phone)
+            startActivity(intent)
+        }
+
+        view.page_button.setOnClickListener {
+            val intent = Intent()
+            intent.action = Intent.ACTION_VIEW
+            intent.data =  Uri.parse(myBundle?.reserveUrl)
+            startActivity(intent)
+        }
 
         return view
     }
